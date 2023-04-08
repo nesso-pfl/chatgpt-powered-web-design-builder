b77dab426af92d518b87988fac882f00037ece53
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-04-09 00:23:54 +0900

    Impl to communicate with ChatGPT

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use chatgpt::prelude::*;
+use dotenvy::dotenv;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let message = env::args().nth(1).expect("You must input message.");
+    dotenv().ok();
+    let api_key = env::var("CHATGPT_API_KEY").expect("CHATGPT_API_KEY must be set int .env file");
+    let client =
+        ChatGPT::new(api_key).expect("Failed to create client. You may input invalid API key.");
+    let response = client
+        .send_message(message)
+        .await
+        .expect("Failed to send message. Confirm your network connection.");
+
+    println!("{}", response.message().content);
 }
