5a5025eb8376381582c8a48f2813937687a88945
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-04-09 15:59:52 +0900

    Place direction message into direction_message.txt

--- src/communicate_with_chatgpt.rs
+++ src/communicate_with_chatgpt.rs
@@ -1,22 +1,11 @@
 use chatgpt::prelude::*;
 
 pub async fn communicate_with_chatgpt(api_key: String, message: String) -> String {
-  let mut client =
-ChatGPT::new(api_key).expect("Failed to create client. You may input invalid API key.").new_conversation_directed(r"私は user で、あなたは assistant です。
-私はHTML, CSS, JavaScript を利用して、 Web ページそのものや Web ページのパーツのレイアウトの実装を行いたいです。
-私はどのような Web ページや Web ページのパーツを作りたいかをあなたに尋ねるので、あなたは私の要望に沿った Web ページや Web ページのパーツを作成し、私に提供してください。
-作成した Web ページや Web ページのパーツは、必ず HTML と CSS と JavaScript で実装してください。
-出力する HTML のコードブロックは、必ず「```html」で始まり「```」で終わってください。
-出力する HTML のファイル名は「index.html」としてください。
-出力する CSS のコードブロックは、必ず「```css」で始まり「```」で終わってください。
-出力する CSS のファイル名は「index.css」としてください。
-出力する JavaScript のコードブロックは、必ず「```js」で始まり「```」で終わってください。
-出力する JavaScript のファイル名は「index.js」としてください。
-但し、 JavaScript は使用する必要がなければ使用しなくても構いません。
-可能な限り JavaScript を使わずに実装してください。
-出力する HTML は、必ず出力する CSS と出力する JavaScript とを読み込むようにしてください。
-出力する JavaScript がない場合、出力する HTML は JavaScript を読み込まないでください。
-");
+  let direction_message = include_str!("./direction_message.txt");
+
+  let mut client = ChatGPT::new(api_key)
+    .expect("Failed to create client. You may input invalid API key.")
+    .new_conversation_directed(direction_message);
   let response = client
     .send_message(message)
     .await
