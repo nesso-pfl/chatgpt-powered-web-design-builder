96bfc0119231eae5392fa5f78e8f63cac8d1ab65
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-04-10 00:22:07 +0900

    Tweaks

--- src/config.rs
+++ src/config.rs
@@ -22,13 +22,11 @@ impl Default for Config {
   fn default() -> Self {
     dotenv().ok();
     Self {
-      chrome: env::var("CHROME")
-        .map(|var| var.parse::<bool>().unwrap_or(false))
-        .unwrap_or(false),
+      chrome: env::var("CHROME").map_or(false, |var| var.parse::<bool>().unwrap_or(false)),
       dir: env::var("CHAT_ID").unwrap_or("default".to_string()),
-      engine: env::var("ENGINE")
-        .map(|var| Engine::from_str(&var).unwrap_or(Engine::GPT3_5))
-        .unwrap_or(Engine::GPT3_5),
+      engine: env::var("ENGINE").map_or(Engine::GPT3_5, |var| {
+        Engine::from_str(&var).unwrap_or(Engine::GPT3_5)
+      }),
       parse_only: false,
     }
   }
